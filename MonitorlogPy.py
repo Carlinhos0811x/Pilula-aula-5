578fbd675366da5e34786a345f1997e027b944b6
Carlinhos0811x <carlinhos0811x@gmail.com>
2026-04-04 14:43:24 -0300

    Update MonitorlogPy.py

--- MonitorlogPy.py
+++ MonitorlogPy.py
@@ -1,43 +1,64 @@
 import random
 import datetime
 
+
 def menu():
+    nome_arq = 'log.txt'
+
     while True:
-        nome_arq = 'log,txt'
-        while True:
-            print('MENU|n')
-            print('1 - Gerar logs')
-            print('2 - Analisar logs')
-            print('3 - Gerar e Analisar logs')
-            print('SAIR')
-            opc = int(input('Escolher uma opção:'))
-            if opc == 1:
-                try:
-                    qtd = int(input('Qualidade de logs (resgistro):'))
-                    gerarArquivo(nome_arq, qtd)
-                except:
-                    print(('Entrada invalida'))
-            elif opc == 2:
-                try:
-                    qtd = int(input('Quantidade de logs(registro):'))
-                    gerarArquivo(nome_arq, qtd)
-                    analisarlogs(nome_arq, qtd)
-                except:
-                    print('Entrada invalida')
-            elif opc == 4:
-                print("até mais")
-                break
-            else
-                print('Opção invalida')
-                
+        print('\nMENU')
+        print('1 - Gerar logs')
+        print('2 - Analisar logs')
+        print('3 - Gerar e Analisar logs')
+        print('4 - SAIR')
+
+        try:
+            opc = int(input('Escolher uma opção: '))
+        except ValueError:
+            print('Entrada inválida')
+            continue
+
+        if opc == 1:
+            try:
+                qtd = int(input('Quantidade de logs (registros): '))
+                gerarArquivo(nome_arq, qtd)
+            except ValueError:
+                print('Entrada inválida')
+
+        elif opc == 2:
+            try:
+                qtd = int(input('Quantidade de logs (registros): '))
+                gerarArquivo(nome_arq, qtd)
+                analisarLogs(nome_arq, qtd)
+            except ValueError:
+                print('Entrada inválida')
+
+        elif opc == 3:
+            try:
+                qtd = int(input('Quantidade de logs (registros): '))
+                gerarArquivo(nome_arq, qtd)
+                analisarLogs(nome_arq, qtd)
+            except ValueError:
+                print('Entrada inválida')
+
+        elif opc == 4:
+            print('Até mais')
+            break
+
+        else:
+            print('Opção inválida')
+
+
 def gerarArquivo(nome_arq, qtd):
-    with open(nome_arq,"w",encoding="UTF-8") as arq:
+    with open(nome_arq, "w", encoding="UTF-8") as arq:
         for i in range(qtd):
-            arq.while(montarlog(i)+"|n")
-            print('log gerado')
-def montarlog(i):
-    data = gerardata(i)
-    ip = gerarip(i)
+            arq.write(montarLog(i) + "\n")
+            print('Log gerado')
+
+
+def montarLog(i):
+    data = gerarData(i)
+    ip = gerarIp(i)
     recurso = gerarRecurso(i)
     metodo = gerarMetodo(i)
     status = gerarStatus(i)
@@ -45,19 +66,56 @@ def montarlog(i):
     agente = gerarAgente(i)
     protocolo = gerarProtocolo(i)
     tamanho = gerarTamanho(i)
-    return f'[{data}] {ip} -{metedo} - {status} - {recurso} - {tempo}ms - {tamanho} - {protocolo} - {agente} - /home'
+
+    return f'[{data}] {ip} - {metodo} - {status} - {recurso} - {tempo}ms - {tamanho} - {protocolo} - {agente} - /home'
+
 
 def gerarData(i):
     base = datetime.datetime.now()
-    delta = datetime.timedelta(seconds= i * random.randint(5,20) )
-    return (base + delta).strftime('%d/%m/%y/ %h:%M:%S')
+    delta = datetime.timedelta(seconds=i * random.randint(5, 20))
+    return (base + delta).strftime('%d/%m/%y %H:%M:%S')
+
 
 def gerarIp(i):
     if i >= 20 and i <= 50:
         return '203.120.45.7'
     else:
-        return f 
-    
+        return f'192.168.0.{random.randint(1, 254)}'
 
-                    
-                              
+
+# ============================
+# FUNÇÕES AINDA NÃO IMPLEMENTADAS
+# (mantidas apenas para o código funcionar)
+# ============================
+
+def gerarRecurso(i):
+    return '/index.html'
+
+
+def gerarMetodo(i):
+    return 'GET'
+
+
+def gerarStatus(i):
+    return 200
+
+
+def gerarTempo(i):
+    return random.randint(10, 500)
+
+
+def gerarAgente(i):
+    return 'Mozilla/5.0'
+
+
+def gerarProtocolo(i):
+    return 'HTTP/1.1'
+
+
+def gerarTamanho(i):
+    return random.randint(200, 5000)
+
+
+def analisarLogs(nome_arq, qtd):
+    # TODO: implementar análise
+    print('Análise ainda não implementada')

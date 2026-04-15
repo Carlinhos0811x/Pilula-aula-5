37c2005296ae25b934a168da48564a9dae3f66c8
Carlinhos0811x <carlinhos0811x@gmail.com>
2026-04-15 20:16:46 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,11 @@
+meses = ['jan','fev','mar','abr','mai','jun',
+         'jul','ago','set','out','nov','dez']
+
+soma = 0
+for i in range(12):
+    salario = float(input(f'Digite o salario de {meses[i]}:'))
+    soma = soma + salario
+    
+sal13 = soma/12
+ferias = sal13 * 1/3
+print(f'13 salario : {sal13}, ferias : {ferias}')

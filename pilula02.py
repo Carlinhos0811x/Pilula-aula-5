4f40ff387c1981f3fd53704f504149b564b86781
Carlinhos0811x <carlinhos0811x@gmail.com>
2026-04-15 13:09:45 -0300

    Update pilula02.py

--- pilula02.py
+++ pilula02.py
@@ -1,12 +1,15 @@
 def simularCrescimento(pop, taxa, limite):
     anos = 0
     while pop <= limite:
-        pop = pop * (1+taxa/100)
+        pop = pop * (1 + taxa / 100)
         anos += 1
     return anos
 
-#main 
-p = float(input('População:'))
-t = float(input('taxa (%)'))
-1 = float(input('Limite: '))
-print('fAnos = {simularCrescimento(p,t,1)}')
+
+# main
+p = float(input('População: '))
+t = float(input('Taxa (%): '))
+l = float(input('Limite: '))
+
+anos = simularCrescimento(p, t, l)
+print(f'Anos = {anos}')
